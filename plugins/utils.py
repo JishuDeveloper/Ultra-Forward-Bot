09478ee708c428016d8e74a6dfe1762d52d306c9
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-03-16 20:08:13 +0530

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -59,8 +59,23 @@ class STS:
                 'keywords': configs['keywords'], 'media_size': size, 'extensions': configs['extension'], 'skip_duplicate': duplicate}, configs['protect'], button
         
 
-
-
+def get_readable_time(seconds: int) -> str:
+    result = ""
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f"{days}d"
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f"{hours}h"
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f"{minutes}m"
+    seconds = int(seconds)
+    result += f"{seconds}s"
+    return result
 
 
 
